cb3dc18f5c8a22868cd10b29a84140e15463bdda
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2022-12-26 13:13:20 +0530

    Math methods

---
+++ Math.js
@@ -0,0 +1,46 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/E
+//math properties
+
+console.log(Math.E) //returns 2.718281828459045
+console.log(Math.LN2);//0.6931471805599453
+console.log(Math.LN10); //2.302585092994046
+
+console.log(Math.LOG2E);//1.4426950408889634  logarithm of e
+console.log(Math.LOG10E);//0.4342944819032518  logarithm of e power 10
+console.log(Math.PI)//3.141592653589793
+console.log(Math.PI.toFixed(2))//3.14
+console.log(Math.sqrt(4)) //2
+console.log(Math.SQRT1_2) //0.7071067811865476 SQR ROOT OF 1/2
+
+console.log("")
+console.log('absolute method');
+console.log(Math.abs(2-10)); //returns 8
+
+console.log('')
+console.log('math methods');
+console.log(Math.trunc(Math.PI)); //returns 3 (removes all decimal)
+console.log(Math.round(3.68)); //returns 4
+console.log(Math.ceil(3.2)); //returns 4 no matter what the deciaml is
+console.log(Math.floor(3.9)); //returns 3 no matter what the deciaml is
+
+console.log("");
+console.log("POW METHOD pow(base,power)");
+console.log(Math.pow(2,3))//8
+
+console.log("");
+console.log("min method");
+console.log(Math.min(2,3,10,28,12,0))//0
+
+console.log("");
+console.log("max method");
+console.log(Math.max(2,3,10,28,12,0))//28
+
+console.log("");
+console.log("random method");
+console.log(Math.random())//0-1 excluding 1
+
+console.log("");
+console.log("logarithmic method");
+console.log(Math.log2(8)); //3
+console.log(Math.log10(100));//2
+ 
