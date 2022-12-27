06837dd4aca52356fbab0c2fa3cbc518a1475e95
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2022-12-27 17:31:51 +0530

    Rock-paper-scissor-Challenge

---
+++ Rock_paper_scissor.js
@@ -0,0 +1,6 @@
+const userChoice=prompt("what is your choice (Enter the numbers) \n1.Rock\n2.Paper\n3.Scissor");
+
+const compChoice=Math.floor(Math.random()*3)+1;
+console.log(compChoice)
+
+userChoice===compChoice?alert('Draw'):userChoice==1&&compChoice==2?alert('Comp Wins'):userChoice==2&&compChoice==3?alert('Comp Wins'):userChoice==3&&compChoice==1?alert('Comp Wins'):('User Wins')
